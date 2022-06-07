7153f701f2bfd03047cc05e561f42c737ab8400d
TimReX-22 <tim.reinhart@bluewin.ch>
2022-06-07 17:06:20 +0200

    added PnP Solver and Publisher for Pose

--- src/object_pose_estimation/pole_pose.py
+++ src/object_pose_estimation/pole_pose.py
@@ -7,15 +7,18 @@ import numpy as np
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 
-
 import tensorflow as tf
 from tensorflow import keras
 import imgaug.augmenters as iaa
 
 from geometry_msgs.msg import PointStamped
+from geometry_msgs.msg import PoseStamped
 
+import getpass
 
-def _to_msg(keypoint, time) -> PointStamped:
+USERNAME = getpass.getuser()
+
+def kp_to_msg(keypoint, time) -> PointStamped:
     msg = PointStamped()
     msg.header.stamp = time
     msg.point.x = keypoint[0]
@@ -23,22 +26,51 @@ def _to_msg(keypoint, time) -> PointStamped:
     msg.point.z = keypoint[2]
     return msg
 
+def get_pose_msg(translation, rotation, time) -> PoseStamped:
+    msg = PoseStamped()
+    msg.header.stamp = time
+    msg.pose.position.x = translation[0]
+    msg.pose.position.y = translation[1]
+    msg.pose.position.z = translation[2]
+    msg.pose.orientation.x = rotation[0]
+    msg.pose.orientation.y = rotation[1]
+    msg.pose.orientation.z = rotation[2]
+    msg.pose.orientation.w = rotation[3]
+    return msg
+
 class PolePoseNode:
     def __init__(self, in_topic, out_topic) -> None:
-        self.model = keras.models.load_model('/home/nico/ComputerVisionBA/Code/BT_Vision/keras_implementation/network')
+        network = "/home/" + USERNAME + "/BT_Vision/keras_implementation/network"
+        self.model = keras.models.load_model(network)
         self.camera = cv2.VideoCapture("/dev/v4l/by-id/usb-e-con_systems_See3CAM_CU55_0C10CE08-video-index0") #Attention: check port nr on udoo!
         if not self.camera.isOpened():
             print("[PolePoseNode] Cannot open camera!")
-        self.keypoints = []
-        self.kp_pub_0 = rospy.Publisher('PifPaf/KeyPoints/1', PointStamped, queue_size=1)
-        self.kp_pub_1 = rospy.Publisher('PifPaf/KeyPoints/2', PointStamped, queue_size=1)
-        self.kp_pub_2 = rospy.Publisher('PifPaf/KeyPoints/3', PointStamped, queue_size=1)
-        self.kp_pub_3 = rospy.Publisher('PifPaf/KeyPoints/4', PointStamped, queue_size=1)
-        self.kp_pub_4 = rospy.Publisher('PifPaf/KeyPoints/5', PointStamped, queue_size=1)
-        self.kp_pub_5 = rospy.Publisher('PifPaf/KeyPoints/6', PointStamped, queue_size=1)
-        self.kp_pub_6 = rospy.Publisher('PifPaf/KeyPoints/7', PointStamped, queue_size=1)
+
+        self.kp_pub_0 = rospy.Publisher('PolePoseNode/KeyPoints/1', PointStamped, queue_size=1)
+        self.kp_pub_1 = rospy.Publisher('PolePoseNode/KeyPoints/2', PointStamped, queue_size=1)
+        self.kp_pub_2 = rospy.Publisher('PolePoseNode/KeyPoints/3', PointStamped, queue_size=1)
+        self.kp_pub_3 = rospy.Publisher('PolePoseNode/KeyPoints/4', PointStamped, queue_size=1)
+        self.kp_pub_4 = rospy.Publisher('PolePoseNode/KeyPoints/5', PointStamped, queue_size=1)
+        self.kp_pub_5 = rospy.Publisher('PolePoseNode/KeyPoints/6', PointStamped, queue_size=1)
+        self.kp_pub_6 = rospy.Publisher('PolePoseNode/KeyPoints/7', PointStamped, queue_size=1)
+
+        self.pose_pub = rospy.Publisher('PolePoseNode/EstimatedPose', PoseStamped, queue_size=1)
 
         self.bridge = CvBridge()
+
+        self.points_3d = np.array([
+                                (0.0, 0.0, 0.0),
+                                (-0.075, 0.0, -0.2),
+                                (0.075, 0.0, -0.2),
+                                (-0.075, 0.0, -0.7),
+                                (0.075, 0.0, -0.7),
+                                (-0.075, 0.0, -1.3),
+                                (-0.075, 0.0, -1.3),
+                                ])
+        self.camera_matrix = np.array([(347.5293999809815 * 244/640, 0.0, 314.7548267525618 * 244/640),
+                                     (0.0, 347.45033648440716 * 244/480, 247.32551331252066 * 244/480),
+                                     (0.0, 0.0, 1.0)])
+        self.dist_coeffs = np.array([-0.06442475368146962, 0.10266027381230053, -0.16303799346444728, 0.08403964035356283])
 
     def __call__(self) -> None:
 
@@ -50,19 +82,25 @@ class PolePoseNode:
         img = cv2.resize(img, (224,224), interpolation = cv2.INTER_LINEAR)
         img = np.expand_dims(img, axis=0)
         predictions = self.model.predict(img).reshape(-1, 7, 2) * 224
-        #predictions, gt_anns, image_meta = self.predictor.numpy_image(img) #dtype predicitions: list
+        keypoints = []
         if len(predictions) == 0:
             print("Did not find any Keypoints!")
         else:
-            for pred in predictions:
-                #print(pred)
-                self.keypoints.append(pred)
-            self.publish(keypoints)
+            for pred in predictions[0]:
+                keypoints.append(pred)
+            self.publish_kp(keypoints)
+            self.estimate_pose(keypoints)
 
-    def publish(self, keypoints) -> None:
+    def estimate_pose(self, keypoints):
+        success, rotation_vec, translation_vec = cv2.solvePnP(self.points_3d, keypoints, self.camera_matrix, self.dist_coeffs)
+        pose_msg = get_pose_msg(translation_vec, rotation_vec, rospy.Time(0))
+        pose_pub.publish(translation_msg)
+
+
+    def publish_kp(self, keypoints) -> None:
         i = 0
         for kp in self.keypoints:
-            kp_msg = _to_msg(kp, rospy.Time(0))
+            kp_msg = kp_to_msg(kp, rospy.Time(0))
             getattr(self, f'kp_pub_{i}').publish(kp_msg)
             i += 1
             if i == 7:
