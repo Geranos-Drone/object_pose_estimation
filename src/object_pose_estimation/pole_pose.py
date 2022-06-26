b63ae62bb6805375978a2a0b34eb44563d643307
TimReX-22 <tim.reinhart@bluewin.ch>
2022-06-26 13:51:55 +0200

    added new network

--- src/object_pose_estimation/pole_pose.py
+++ src/object_pose_estimation/pole_pose.py
@@ -35,6 +35,7 @@ def kp_to_msg(keypoint, time) -> PointStamped:
 def get_pose_msg(translation, rotation, time) -> PoseStamped:
     msg = PoseStamped()
     msg.header.stamp = time
+    msg.header.frame_id = "cam"
     rotation = rotation.flatten()
     print(rotation)
     rotation_obj = R.from_euler('xyz', rotation)       # VERIFY THAT PNP RETURNS ROTATION AS XYZ AND NOT ZXY
@@ -51,11 +52,11 @@ def get_pose_msg(translation, rotation, time) -> PoseStamped:
 
 class PolePoseNode:
     def __init__(self, in_topic, out_topic) -> None:
-        # network = "/home/" + USERNAME + "/BT_Vision/keras_implementation/network"
+        network = "/home/" + USERNAME + "/BT_Vision/outputs/" + "mobilenetv2-220622-202035-pole_detect.pkl.epoch900"
         # self.model = keras.models.load_model(network)
         self.camera = cv2.VideoCapture("/dev/v4l/by-id/usb-e-con_systems_See3CAM_CU55_0C10CE08-video-index0") #Attention: check port nr on udoo!
 
-        self.predictor = openpifpaf.Predictor(checkpoint='/home/' + USERNAME + '/BT_Vision/outputs/mobilenetv2-220611-165917-pole_detect.pkl.epoch339')
+        self.predictor = openpifpaf.Predictor(checkpoint=network)
         cv2.namedWindow('keypoints')
 
         if not self.camera.isOpened():
